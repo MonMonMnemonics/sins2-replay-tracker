4954910441cab48493ffc2f26224f82d56b02285
Cendikia Abdi <cendekiaabdi@gmail.com>
2026-04-20 19:44:42 +0900

    add disclaimer

--- routes/index.tsx
+++ routes/index.tsx
@@ -28,7 +28,7 @@ export default define.page(function Home() {
                     
                     <hr class="my-4"/>
                     <div class="flex items-center justify-between">
-                        <span class="text-white"></span>
+                        <span class="text-gray-300 text-xs">Disclaimer: This project is a fan-made project<br/> and is not affiliated with Ironclad Games.</span>
                         <a class="flex items-center gap-[6px] text-white no-underline transition-colors hover:text-white/70 cursor-pointer" href="https://github.com/MonMonMnemonics/sins2-replay-tracker" target="_blank">
                             <svg class="w-4 h-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
